b7c1b0c0d21312326b25554af8a4a1b518b886d6
Scott Contini <scontini@atlassian.com>
2021-10-06 16:30:10 +1100

    initial server code, and some refactoring of github provider code

--- src/app.py
+++ src/app.py
@@ -1,5 +1,26 @@
 from flask import Flask, request
 import json
+import os
+import sys
+from providers import github
+
+
+if 'token' in os.environ:
+    token = os.environ['token']
+else:
+    print("I require an access token to work")
+    print("try: export token=...")
+    sys.exit()
+
+def output_results( results ):
+    if results == []:
+        print("\nMate, I tried the best I could, but couldn't find any secrets :-(\n")
+    else:
+        print("\n*** Here are the findings: ***\n")
+        for result in results:
+            print( json.dumps(result, indent = 2) )
+            print( "----------------------------------------" )
+
 
 app = Flask(__name__)
 
@@ -10,9 +31,16 @@ def heartbeat():
 
 @app.route("/github_push", methods=['POST'])
 def github_post_webhook():
-    print (request.is_json)
+    # TODO: verify signature
     content = request.get_json()
-    print(json.dumps( content, indent = 2))
+    all_results = []
+    for commit in content["commits"]:
+        commit_url = commit["url"]
+        print(commit_url)
+        results = github.process_single_commit( token, commit_url )
+        all_results = all_results + results
+    output_results(all_results)
+    # print(json.dumps( content["commits"], indent = 2))
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 
--- src/providers/github.py
+++ src/providers/github.py
@@ -6,14 +6,33 @@ import requests
 from scanner import scan_diff
 from rules import read_rules
 
+def process_single_commit( token, commit_url ):
+
+    # print(commit)
+    diff_url = commit_url + ".diff"
+    print(diff_url)
+    # TODO: token will need to be sent in to access private git repos
+    r = requests.get( diff_url )
+    diff_text = r.text
+    if len(diff_text) < 1048576:
+        results = scan_diff( r.text )
+        for result in results:
+            result["commit_url"] = commit_url
+    else:
+        print("Diff is huge, will skip this one")
+    print("----------------------------------------")
+    return results
+
+
+
 def process_repo( token, repo ):
     # login to github with our token
     g = Github(token)
 
     if repo == None:
         # We're going to grab the first repo for this user.
-        print("No repo provided, so I'll choose a repo from this user")
-        repo = g.get_user().get_repos()[0]
+        print("No repo provided, sorry can't help you.")
+        sys.exit()
     else:
         repo = g.get_repo(repo)
 
@@ -23,18 +42,7 @@ def process_repo( token, repo ):
     all_results = []
     for commit in repo.get_commits( ):
         # print(commit)
-        diff_url = commit.html_url+".diff"
-        print(diff_url)
-        r = requests.get( diff_url )
-        diff_text = r.text
-        if len(diff_text) < 1048576:
-            # results = scan_diff( r.text, rules )
-            results = scan_diff( r.text )
-            # TODO: insert commit URL into each result
-            for result in results:
-                result["commit_url"] = commit.html_url
-            all_results = all_results + results
-        else:
-            print("Diff is huge, will skip this one")
-        print("----------------------------------------")
+        results = process_single_commit( token, commit.html_url )
+        all_results = all_results + results
     return all_results
+
